fc7f6fc0c196b954943623dbd819fdb8e04a5d38
Anija <anija.nair@yahoo.com>
2017-11-04 00:03:13 -0400

    'Author' model created and migrated

--- famous_authors/authors/models.py
+++ famous_authors/authors/models.py
@@ -6,3 +6,7 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=255)
     about = models.TextField()
+
+
+def __str__(self):
+    return self.name
--- famous_authors/authors/views.py
+++ famous_authors/authors/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Author
 
-# Create your views here.
+# Create views here.
+
+def author_list(request):
+    authors = Author.objects.all()
+    output = ', '.join([str(author) for author in authors])
+    return HttpResponse(output)
